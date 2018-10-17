a89f9cf485f3392b14ef52f17ad72228ff67cf10
VanLiuZhi <1441765847@qq.com>
2018-10-17 19:18:58 +0800

    init

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 代码高亮
+import VueHighlightJS from 'vue-highlight.js';
+// import 'highlight.js/styles/atom-one-dark.css';
+import 'highlight.js/styles/agate.css'
+
+const helper = require('./../helper/')
+
+export default ({
+  Vue, // VuePress 正在使用的 Vue 构造函数
+  options, // 附加到根实例的一些选项
+  router, // 当前应用的路由实例
+  siteData // 站点元数据
+}) => {
+  /* @tip: 可以在此，做一些其他的应用级别的优化 */
+
+  Vue.use(Element)
+  Vue.use(VueHighlightJS)
+  helper.utils.rearrangeTheSidebars(siteData)
+}
